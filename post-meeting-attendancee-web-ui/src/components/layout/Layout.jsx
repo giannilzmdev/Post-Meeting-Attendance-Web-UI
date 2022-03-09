dae03f4949c9a464f55d3bb86fcf1ec4e2857c9c
Gianni <giannilatangezmdev@gmail.com>
2022-03-09 14:20:05 -0500

    add sidebar css

--- post-meeting-attendancee-web-ui/src/components/layout/Layout.jsx
+++ post-meeting-attendancee-web-ui/src/components/layout/Layout.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import Sidebar from '../sidebar/Sidebar'
 import LinkPaths from '../LinkPaths'
+import './layout.css'
 import { BrowserRouter } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <BrowserRouter>
-      
+      <div className='layout'>
        <Sidebar/>
-       <LinkPaths/>
-
+       <div className='layout__content'>
+         <div className='layout__content-main'>
+           <LinkPaths/>
+         </div>
+       </div>
+      </div>
     </BrowserRouter>
   )
 }
--- post-meeting-attendancee-web-ui/src/components/sidebar/Sidebar.jsx
+++ post-meeting-attendancee-web-ui/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './sidebar.css'
 import { Link } from 'react-router-dom'
 import LinkPaths from '../LinkPaths'
 
