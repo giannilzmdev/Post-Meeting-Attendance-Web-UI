0f11a93e1660685e097e9061014d413415957166
Gianni <giannilatangezmdev@gmail.com>
2022-05-05 16:45:00 -0400

    map all participant info

--- post-meeting-attendancee-web-ui/src/pages/PastParticipants.jsx
+++ post-meeting-attendancee-web-ui/src/pages/PastParticipants.jsx
@@ -9,9 +9,22 @@ const PastParticipants = () => {
         // Using && symbol means to check if data exists before mapping so page doesn't break.
         Participants && Participants.map( participant => {
           return(
-            <div className="meeting">
+            <div className="meeting" >
               { participant.next_page_token } <br/>
               { participant.page_count} <br/>
+              { participant.page_size} <br/>
+              { participant.participants.map(data =>{
+                return(
+                  <ul>
+                    <li> Id: {data.id}</li>
+                    <li> Name: {data.name}</li>
+                    <li> Email: {data.user_email}</li>
+
+                  </ul>
+                )
+              
+              })}
+             
 
              </div> 
           ) 
