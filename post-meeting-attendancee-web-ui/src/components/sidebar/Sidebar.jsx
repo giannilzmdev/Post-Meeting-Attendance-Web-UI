7dc05e35f95dd874e74091896f82081075b73a7a
Gianni <giannilatangezmdev@gmail.com>
2022-06-22 19:02:21 -0400

    complete sidebar css

--- post-meeting-attendancee-web-ui/src/components/sidebar/Sidebar.jsx
+++ post-meeting-attendancee-web-ui/src/components/sidebar/Sidebar.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import './sidebar.css'
 import { Link } from 'react-router-dom'
-import LinkPaths from '../LinkPaths'
+import './sidebar.css'
+import logo from '../../assets/images/logo.png'
+import sidebar_items from '../../assets/json-data/SidebarRoutes.json'
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='sidebar'>
+      <div className='sidebar__logo'>
+        <img src={logo} alt='company logo'/>
+      </div>
+      {
+        sidebar_items.map((item, index)=> (
+          <Link to={item.route} key={index}>
+            <div>
+              {item.display_name}
+            </div>
+          </Link>
+        ))
+      }
   
-      <nav>
-        <Link to='/'> Dashboard </Link>
-        <Link to='/past_participants'> Past Participants </Link>
-        <Link to='/past_meetings'> Past Meetings </Link>
-      </nav>
+
 
     </div>
 
@@ -20,3 +30,10 @@ const Sidebar = () => {
 }
 
 export default Sidebar
+
+// Icons for css from https://boxicons.com/
+{/* <nav>
+<Link to='/'> Dashboard </Link>
+<Link to='/past_participants'> Past Participants </Link>
+<Link to='/past_meetings'> Past Meetings </Link>
+</nav> */}
